100a24a6c54900eac606b6ee5981464fb932b580
sebastianZW20501 <sebastianzepedawaissman@gmail.com>
2025-05-12 09:33:41 -0600

    Update msquares.c

--- msquares/msquares.c
+++ msquares/msquares.c
@@ -2,65 +2,46 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
-#include "msquares.c"
 
+typedef struct {
+  short col, row;
+} Pos;
 
-// State Definitions
-typedef enum {
-    STATE_SLEEP,
-    STATE_IDLE,
-    STATE_HOURGLASS,
-    STATE_BALL
-} State;
+Pos positions[] = {
+  {10,10},			/* upper left */
+  {10, screenHeight-10}, 	/* lower left */
+  {screenWidth - 10, screenHeight-10}, /* lower right */
+  {screenWidth - 10, 10},	       /* upper right */
+  {screenWidth/2, screenHeight/2}      /* middle */
+};
+#define NUM_POSITIONS 5
 
-State currentState = STATE_SLEEP;
+unsigned short sqColors[] = {COLOR_RED, COLOR_GREEN, COLOR_ORANGE, COLOR_BLUE};
+#define NUM_SQCOLORS 4
+#define BG_COLOR COLOR_BLACK
 
-// Switches
-#define SW1 1
-#define SW2 2
-#define SW3 4
-#define SW4 8
+char current_position = 0, current_color = 0;
+  
+// WARNING: LCD DISPLAY USES P1.0.  Do not touch!!! 
+
+#define LED BIT6		/* note that bit zero req'd for display */
+
 #define SWITCHES 15
 
-// LED and Timer
-#define LED BIT6
-short redrawScreen = 1;
+int redrawScreen = 1;
 
-// Ball Movement
-short drawPos[2] = {1, 10}, controlPos[2] = {2, 10};
-short colVelocity = 1, colLimits[2] = {1, 48};
-
-// Hourglass Colors
-char blue = 31, green = 0, red = 31;
-unsigned char step = 0;
-
-// Forward Declarations
-void update_shape();
-void draw_ball(int col, int row, unsigned short color);
-void screen_update_ball();
-void screen_update_hourglass();
-void go_to_sleep();
-
-void switch_interrupt_handler();
-//void configure_clocks();
-//void clearScreen();
-
-// Switch Interrupt Handler
-void __interrupt_vec(PORT2_VECTOR) Port_2() {
-    if (P2IFG & SWITCHES) {
-        P2IFG &= ~SWITCHES;
-        switch_interrupt_handler();
-
-        P1OUT ^= LED;
-
-        // Wake up the CPU from low power mode
-        __bic_SR_register_on_exit(LPM4_bits);
-    }
+static char 
+switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+  /* update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
-
-
-void switch_init()			/* setup switch */
+void 
+switch_init()			/* setup switch */
 {  
   P2REN |= SWITCHES;		/* enables resistors for switches */
   P2IE |= SWITCHES;		/* enable interrupts from switches */
@@ -69,107 +50,103 @@ void switch_init()			/* setup switch */
   switch_update_interrupt_sense();
 }
 
+int switches = 0;
 
+void
+switch_interrupt_handler()
+{
+  char p2val = switch_update_interrupt_sense();
+  switches = ~p2val & SWITCHES;
 
-
-void switch_interrupt_handler() {
-    char p2val = P2IN & SWITCHES;
-    if (p2val & SW1) currentState = STATE_HOURGLASS;
-    if (p2val & SW2) currentState = STATE_BALL;
-    if (p2val & SW4) go_to_sleep();
+  if (switches & SWITCHES) { 	/* a switch is depresssed */
     redrawScreen = 1;
+    for (char swNum = 0; swNum < 4; swNum++) { /* respond to lowest button pressed */
+      int swFlag = 1 << swNum;
+      if (switches & swFlag) {
+	current_position = swNum;
+	break;
+      }
+    }
+  }
 }
 
-// Draw Ball
-void draw_ball(int col, int row, unsigned short color) {
-    // Replace this with your fillRectangle function
+void wdt_c_handler()
+{
+  static int sec2Count = 0;
+  static int sec1Count = 0;
+  if (sec2Count++ >= 125) {		/* 2/sec */
+    sec2Count = 0;
+    current_color = (current_color+1) % NUM_SQCOLORS;
+    redrawScreen = 1;
+  }
+  if (sec1Count++ >= 250) {		/* 1/sec */
+    sec1Count = 0;
+    current_position = (current_position+1) % NUM_POSITIONS;
+    redrawScreen = 1;
+  }
+}
+  
+void update_shape();
+
+void main()
+{
+  
+  P1DIR |= LED;		/**< Green led on when CPU on */
+  P1OUT |= LED;
+  configureClocks();
+  lcd_init();
+  switch_init();
+  
+  enableWDTInterrupts();      /**< enable periodic interrupt */
+  or_sr(0x8);	              /**< GIE (enable interrupts) */
+  
+  clearScreen(BG_COLOR);
+  while (1) {			/* forever */
+    if (redrawScreen) {
+      redrawScreen = 0;
+      update_shape();
+    }
+    P1OUT &= ~LED;	/* led off */
+    or_sr(0x10);	/**< CPU OFF */
+    P1OUT |= LED;	/* led on */
+  }
 }
 
-// Update Ball Screen
-void screen_update_ball() {
-    for (char axis = 0; axis < 2; axis++) 
-        if (drawPos[axis] != controlPos[axis]) 
-            goto redraw;
+    
+    
+void
+update_shape()
+{
+  static char last_position = 0, last_color = 0;
+  redrawScreen = 0;
+  int pos, color;
+  and_sr(~8);  // mask interrupts (GIE = 0)
+  pos = current_position;	/* read state variables */
+  color = current_color;
+  or_sr(8); // unmask interrupts
+  if (pos == last_position && color == last_color) /* nothing to redraw */
     return;
-redraw:
-    draw_ball(drawPos[0], drawPos[1], 0x001F); // Erase (replace with your color code)
-    for (char axis = 0; axis < 2; axis++) 
-        drawPos[axis] = controlPos[axis];
-    draw_ball(drawPos[0], drawPos[1], 0xFFFF); // Draw (replace with your color code)
-}
 
-// Update Hourglass Screen
-void screen_update_hourglass() {
-    clearScreen(COLOR_BLUE);
-
-    static unsigned char row = screenHeight / 2, col = screenWidth / 2;
-    static char lastStep = 0;
-
-    for (lastStep = 0; lastStep <= 30; lastStep++) {
-        int startCol = col - lastStep;
-        int endCol = col + lastStep;
-        int width = 1 + endCol - startCol;
-
-        unsigned int color = (blue << 11) | (green << 5) | red;
-
-        fillRectangle(startCol, row + lastStep, width, 1, color);
-        fillRectangle(startCol, row - lastStep, width, 1, color);
-    }
+  /* erase old shape */
+  short col = positions[last_position].col;
+  short row = positions[last_position].row;
+  if (pos != last_position)    /* erase if position changed */
+    fillRectangle(col-5, row-5, 10, 10, BG_COLOR); 
+  /* draw new shape */
+  col = positions[pos].col;
+  row = positions[pos].row;
+  fillRectangle(col-5, row-5, 10, 10, sqColors[color]); /* draw new shape */
+  /* remember color & pos for next redraw */
+  last_position = pos;
+  last_color = color;
 }
 
 
-// Main Update Shape Logic
-void update_shape() {
-    switch (currentState) {
-        case STATE_HOURGLASS:
-            screen_update_hourglass();
-            break;
-        case STATE_BALL:
-            screen_update_ball();
-            break;
-        case STATE_SLEEP:
-            go_to_sleep();
-            break;
-        default:
-            break;
-    }
-}
-
-// Enter Low Power Mode
-void go_to_sleep() {
-    or_sr(0x18); // CPU Off, GIE enabled
-}
-
-
-
-
-// Main Entry Point
-void main() {
-    // Initialize the clock and the LED
-    configureClocks();
-    P1DIR |= LED;   // Set LED as output
-    P1OUT |= LED;   // Turn it on to indicate startup
-
-    // Initialize the screen and the buttons
-    lcd_init();
-    clearScreen(COLOR_BLUE);
-    switch_init();
-
-
-    // Set the starting state to sleep
-    currentState = STATE_SLEEP;
-
-    // Enable interrupts and low-power mode
-    enableWDTInterrupts();    // Watchdog timer interrupts
-    or_sr(0x18);              // GIE (Global Interrupt Enable) + CPU off
-
-    while (1) {
-        if (redrawScreen) {
-            redrawScreen = 0;
-            update_shape();
-        }
-
-        // Low power mode wait
-        __bis_SR_register(LPM0_bits + GIE); // Enter LPM0 (CPU off, peripherals on)
-    }
+/* Switch on S2 */
+void
+__interrupt_vec(PORT2_VECTOR) Port_2(){
+  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
 }
