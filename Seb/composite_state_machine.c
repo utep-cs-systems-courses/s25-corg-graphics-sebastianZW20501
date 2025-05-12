6f583992a0dcbc36846bec11dce781903820d844
sebastianZW20501 <sebastianzepedawaissman@gmail.com>
2025-05-12 01:17:00 -0600

    Update composite_state_machine.c

--- Seb/composite_state_machine.c
+++ Seb/composite_state_machine.c
@@ -70,6 +70,16 @@ void switch_init()			/* setup switch */
 
 
 
+switch_update_interrupt_sense()
+{
+  char p2val = P2IN;
+  /* update switch interrupt to detect changes from current buttons */
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
+}
+
+
 
 void switch_interrupt_handler() {
     char p2val = P2IN & SWITCHES;
