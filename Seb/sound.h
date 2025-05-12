26c80b2f2c93845607608f7cd876fe1a3a285a15
sebastianZW20501 <sebastianzepedawaissman@gmail.com>
2025-05-11 22:57:30 -0600

    Delete Seb directory

--- Seb/sound.h
+++
@@ -1,9 +0,0 @@
-#ifndef sound_included
-#define sound_included
-
-void buzzer_init();
-void play_sound(int freq);
-void stop_sound();
-
-#endif
-
--- Seb/stateMachine.h
+++
@@ -1,9 +0,0 @@
-#ifndef statemachine_included
-#define statemachine_included
-
-void state_advance();
-
-extern int current_state;
-
-#endif
-
